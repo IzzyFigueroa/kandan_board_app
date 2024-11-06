345e087a129896014209cbf3e2a964ad0f0c0656
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-11-05 22:18:58 -0500

    almost done

---
+++ server/dist/routes/auth-routes.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+export const login = async (req, res) => {
+    console.log('JWT_SECRET:', process.env.JWT_SECRET);
+    const { username, password } = req.body;
+    // Find the user by username
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    // Compare the provided password with the stored hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    // Generate a JWT token
+    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    // Return the token
+    return res.json({ token });
+};
+const router = Router();
+// POST /login - Login a user
+router.post('/login', login);
+export default router;
---
+++ server/src/routes/auth-routes.ts
@@ -0,0 +1,38 @@
+import { Router, Request, Response } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+
+
+export const login = async (req: Request, res: Response) => {
+  
+  const { username, password } = req.body;
+  
+    // Find the user by username
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    // Compare the provided password with the stored hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+
+    // Return the token
+    return res.json({ token });
+    
+  } 
+
+
+const router = Router();
+
+// POST /login - Login a user
+router.post('/login', login);
+
+export default router;
