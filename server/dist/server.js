6d334349e30efaf333fba47e13cbd0baa3a9a2ca
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-11-06 09:27:18 -0500

    ready for deploy

--- server/dist/server.js
+++ server/dist/server.js
@@ -4,12 +4,22 @@ dotenv.config();
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { dirname } from 'path';
 const app = express();
 const PORT = process.env.PORT || 3001;
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
 app.use(express.json());
 app.use(routes);
+if (process.env.PORT) {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    });
+}
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
     app.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`);
